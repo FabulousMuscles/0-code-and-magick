2a6228a01365b309bd8ebe85671f44eaf05aa26d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-02-12 16:51:03 +0300

    use `util.promisify`

--- src/generate.js
+++ src/generate.js
@@ -1,5 +1,7 @@
 const {generate} = require(`./generator/wizards-generator`);
 const fs = require(`fs`);
+const util = require(`util`);
+const writeFile = util.promisify(fs.writeFile);
 
 const fileWriteOptions = {encoding: `utf-8`, mode: 0o644};
 const data = generate();
@@ -8,13 +10,6 @@ module.exports = {
   name: `generate`,
   description: `Generates data for project`,
   execute(filePath = `${process.cwd()}/wizards-data.json`) {
-    return new Promise((success, fail) => {
-      fs.writeFile(filePath, JSON.stringify(data), fileWriteOptions, (err) => {
-        if (err) {
-          return fail(err);
-        }
-        return success();
-      });
-    });
+    return writeFile(filePath, JSON.stringify(data), fileWriteOptions);
   }
 };
